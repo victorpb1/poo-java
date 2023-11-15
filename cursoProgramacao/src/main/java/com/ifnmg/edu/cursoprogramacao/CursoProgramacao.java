dd177ff72b2654f4dcbc25c6588f6711636b1733
Victor Pereira Barbosa <victorpbarbosa1@gmail.com>
2023-11-15 10:12:28 -0300

    cursoJava-pasta

---
+++ cursoProgramacao/src/main/java/com/ifnmg/edu/cursoprogramacao/CursoProgramacao.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ifnmg.edu.cursoprogramacao;
+
+/**
+ *
+ * @author Victor
+ */
+public class CursoProgramacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Teste de velocidade");
+        
+    }
+}
